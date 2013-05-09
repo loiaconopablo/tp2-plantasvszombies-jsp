f59b4c657745f9f6cdbe81ba6600cb9ece5db0e5
loiaconopablo@gmail.com <loiaconopablo@gmail.com@1bfea99e-fac3-9280-8891-d86aefabe69a>
2013-05-09 00:42:22 +0000

    Primeros cambios.

---  tp2-plantasvszombies-jsp/JSP-PlantasVSZombies/src/main/java/controllers/SearchServlet.java
+++  tp2-plantasvszombies-jsp/JSP-PlantasVSZombies/src/main/java/controllers/SearchServlet.java
@@ -3,12 +3,36 @@
  */
 package controllers;
 
+import java.io.IOException;
+import java.util.Collection;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.uqbar.commons.model.UserException;
+
 
 /**
- * @author usuario
+ * @author Mariano Varela, Pablo Loiacono.
  *
  */
 public class SearchServlet extends HttpServlet {
+	
+	protected void doPost(HttpServletRequest request, 
+			HttpServletResponse response) 
+			throws ServletException, IOException {
+		String titulo = request.getParameter("nombre");
+		
+//		Collection<Libro> libros = Biblioteca.getInstance().buscar(titulo);
+//		request.getSession().setAttribute("libros", libros);
+//		request.getRequestDispatcher("index.jsp").forward(request, response);
+	}
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) 
+			throws ServletException, IOException {
+		this.doPost(request, response);
+	}
+	
+	
 
 }
