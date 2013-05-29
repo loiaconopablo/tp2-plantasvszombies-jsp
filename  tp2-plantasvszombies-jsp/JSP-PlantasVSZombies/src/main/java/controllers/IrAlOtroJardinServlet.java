11702a0e125cc45e11ff54a233cacc2aa6137570
marianogvarela@gmail.com <marianogvarela@gmail.com@1bfea99e-fac3-9280-8891-d86aefabe69a>
2013-05-29 21:31:14 +0000

    Version final

---  tp2-plantasvszombies-jsp/JSP-PlantasVSZombies/src/main/java/controllers/IrAlOtroJardinServlet.java
+++  tp2-plantasvszombies-jsp/JSP-PlantasVSZombies/src/main/java/controllers/IrAlOtroJardinServlet.java
@@ -4,32 +4,18 @@
 package controllers;
 
 import java.io.IOException;
-import java.util.Collection;
-import java.util.List;
 
 import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import org.uqbar.commons.model.UserException;
 
 import plantasVsZombies.AdministradorJardinZen;
-import plantaszombies.Jardin;
-import plantaszombies.JardinZen;
-import plantaszombies.Partida;
-import plantaszombies.Terreno;
-import plantaszombies.Semilla;
 
 /**
  * @author Mariano Varela, Pablo Loiacono.
  *
  */
-//public class IrAlOtroJardinServlet extends HttpServlet {
 public class IrAlOtroJardinServlet extends SearchServlet{	
-	/**
-	 * 
-	 */
-	//private static final long serialVersionUID = 1L;
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)	throws ServletException, IOException {
 
